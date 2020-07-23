ccddb6511a3193e8f1238e96246a1167687b3d36
sujoy-coder <ask2sujoy@gmail.com>
2020-07-23 21:50:59 +0530

    All Done !

--- App/Server/__init__.py
+++ App/Server/__init__.py
@@ -2,15 +2,15 @@
 # For Connection the App To a Production DataBase
 # Database credentials ...
 
-CLOUDANT_USERNAME = "f1bec71f-2fa9-4699-b4c0-ec55e6308a91-bluemix"
-CLOUDANT_PASSWORD = "c3f8b8c0cc64c6a3237fde1e51647e134ece2cf077f6c10d13d716dde1ed1e75"
-CLOUDANT_URL = "https://f1bec71f-2fa9-4699-b4c0-ec55e6308a91-bluemix:c3f8b8c0cc64c6a3237fde1e51647e134ece2cf077f6c10d13d716dde1ed1e75@f1bec71f-2fa9-4699-b4c0-ec55e6308a91-bluemix.cloudantnosqldb.appdomain.cloud"
+CLOUDANT_USERNAME = "MY SECRET"
+CLOUDANT_PASSWORD = "MY SECRET"
+CLOUDANT_URL = "MY SECRET"
 
 
 
 
 # Admin credentials ...
 
-DATABASE_NAME = "turbineuserdatabase"
+DATABASE_NAME = "turbineuserdata"
 ADMIN_USERNAME = "root"
 ADMIN_PASSWORD = "root"
